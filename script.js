b86341d245ab252520c9b03a3204f54a305a884c
kozhomberdievs <kozhomberdievs@icloud.com>
2026-03-24 17:33:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+var str = "Text"
+var num = 10
+var a
+
+console.log(a); 
+console.log(num > 200)
+console.log(num < 100)
+console.log("text" / num)
+console.log(typeof num)
+console.log(typeof str)
+
+
+var sum = prompt("Введите сумму покупки: ")
+
+if (isNaN(sum) || sum < 0) {
+  console.log("Ошибка: введите корректное положительное число.")
+} else {
+  var discount = 0;
+
+  if (sum < 100){
+    discount = 0.00
+  } else if (sum => 100 && sum < 500) {
+    discount = 0.05
+  } else if (sum => 500 && sum <= 1000) {
+    discount = 0.10
+  } else if (sum > 1000) {
+    discount = 0.15
+  }
+}
+
+var price = sum - (sum * discount)
+
+price = price.toFixed(2);
+
+console.log("Сумма покупки: " + sum + " сом");
+console.log("Ваша скидка: " + (discount * 100) + " сом");
+console.log("Итого к оплате: " + price + " сом");
