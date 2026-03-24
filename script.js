ac469e40ad4656b4a4c80649efec4c4df16f618a
kozhomberdievs <kozhomberdievs@icloud.com>
2026-03-24 17:56:52 +0600

    fix code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var str = "Text";
-var num = 10;
-var a;
+// var str = "Text";
+// var num = 10;
+// var a;
 
-console.log(a);
-console.log(num > 200);
-console.log(num < 100);
-console.log("text" / num);
-console.log(typeof num);
-console.log(typeof str);
+// console.log(a);
+// console.log(num > 200);
+// console.log(num < 100);
+// console.log("text" / num);
+// console.log(typeof num);
+// console.log(typeof str);
 
 var sum = prompt("Введите сумму покупки: ");
 
@@ -18,10 +18,10 @@ if (isNaN(sum) || sum < 0) {
 
   if (sum < 100) {
     discount = 0.0;
-  } else if ((sum) => 100 && sum < 500) {
+  } else if ((sum) >= 100 && sum < 500) {
     discount = 0.05;
-  } else if ((sum) => 500 && sum <= 1000) {
-    discount = 0.10;
+  } else if ((sum) >= 500 && sum <= 1000) {
+    discount = 0.1;
   } else if (sum > 1000) {
     discount = 0.15;
   }
@@ -32,5 +32,5 @@ var price = sum - sum * discount;
 price = price.toFixed(2);
 
 console.log("Сумма покупки: " + sum + " сом");
-console.log("Ваша скидка: " + (discount * sum) + " сом");
+console.log("Ваша скидка: " + sum * discount + " сом");
 console.log("Итого к оплате: " + price + " сом");
