43a1f10b75c5d8cd9076c05077d1a2a404e6e512
kozhomberdievs <kozhomberdievs@icloud.com>
2026-03-27 23:38:08 +0600

    commit 7 dz

--- script.js
+++ script.js
@@ -9,28 +9,30 @@
 // console.log(typeof num);
 // console.log(typeof str);
 
-var sum = prompt("Введите сумму покупки: ");
+// var sum = prompt("Введите сумму покупки: ");
 
-if (isNaN(sum) || sum < 0) {
-  console.log("Ошибка: введите корректное положительное число.");
-} else {
-  var discount = 0;
+// if (isNaN(sum) || sum < 0) {
+//   console.log("Ошибка: введите корректное положительное число.");
+// } else {
+//   var discount = 0;
 
-  if (sum < 100) {
-    discount = 0.0;
-  } else if ((sum) >= 100 && sum < 500) {
-    discount = 0.05;
-  } else if ((sum) >= 500 && sum <= 1000) {
-    discount = 0.1;
-  } else if (sum > 1000) {
-    discount = 0.15;
-  }
-}
+//   if (sum < 100) {
+//     discount = 0.0;
+//   } else if ((sum) >= 100 && sum < 500) {
+//     discount = 0.05;
+//   } else if ((sum) >= 500 && sum <= 1000) {
+//     discount = 0.1;
+//   } else if (sum > 1000) {
+//     discount = 0.15;
+//   }
+// }
 
-var price = sum - sum * discount;
+// var price = sum - sum * discount;
 
-price = price.toFixed(2);
+// price = price.toFixed(2);
 
-console.log("Сумма покупки: " + sum + " сом");
-console.log("Ваша скидка: " + sum * discount + " сом");
-console.log("Итого к оплате: " + price + " сом");
+// console.log("Сумма покупки: " + sum + " сом");
+// console.log("Ваша скидка: " + sum * discount + " сом");
+// console.log("Итого к оплате: " + price + " сом");
+
+
