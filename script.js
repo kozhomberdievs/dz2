09bbadd5bce41b4e889e3eea9d45e5e7f1aca9cc
kozhomberdievs <kozhomberdievs@icloud.com>
2026-03-27 23:40:20 +0600

    fix

--- script.js
+++ script.js
@@ -35,4 +35,55 @@
 // console.log("Ваша скидка: " + sum * discount + " сом");
 // console.log("Итого к оплате: " + price + " сом");
 
+const orders = [
+  {
+    id: 1,
+    customer: "Jhon",
+    items: [
+      { name: "Ноутбук", price: 50000, quantity: 1 },
+      { name: "Мышь", price: 1500, quantity: 2 },
+    ],
+  },
+  {
+    id: 2,
+    customer: "Alan",
+    items: [
+      { name: "Телефон", price: 30000, quantity: 1 },
+      { name: "Чехол", price: 1000, quantity: 3 },
+    ],
+  },
+  {
+    id: 3,
+    customer: "Jane",
+    items: [
+      { name: "Клавиатура", price: 2500, quantity: 1 },
+      { name: "Монитор", price: 12000, quantity: 1 },
+    ],
+  },
+];
 
+
+
+
+let total = 0;       
+let maxSum = 0;            
+let maxCustomer = "";       
+
+for (let i = 0; i < orders.length; i++) {
+  let orderSum = 0;
+
+  for (let j = 0; j < orders[i].items.length; j++) {
+    let item = orders[i].items[j];
+    orderSum += item.price * item.quantity;
+  }
+  console.log("Заказ номер: " + orders[i].id + " (" + orders[i].customer + "): " + orderSum);
+  total += orderSum;
+
+  if (orderSum > maxSum) {
+    maxSum = orderSum;
+    maxCustomer = orders[i].customer;
+  }
+}
+
+console.log("Общий доход:", total);
+console.log("Самый дорогой заказ сделал:", maxCustomer, "-", maxSum);
